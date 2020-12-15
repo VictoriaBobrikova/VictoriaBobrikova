682cb4f33b1dcd7e0f03d8fd65cfad254e7cfe6f
VictoriaBobrikova <bobtory@yandex.ru>
2020-12-15 20:53:22 +0300

    final fix

--- src/test/java/hw1/CalculatorMultiplyTest.java
+++ src/test/java/hw1/CalculatorMultiplyTest.java
@@ -1,19 +1,16 @@
 package hw1;
 
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class CalculatorMultiplyTest extends AbstractCalculatorTest {
 
-
     @Test(dataProvider = "multiplyDataSetDouble", dataProviderClass = TestDataProvider.class)
     public void multiplyDoubleTest(double a, double b, double expected) {
         double actual = calculator.mult(a, b);
         assertEquals(actual, expected, 1);
     }
-
 
     @Test(dataProvider = "multiplyDataSetLong", dataProviderClass = TestDataProvider.class)
     public void multiplyLongTest(long a, long b, long expected) {
--- src/test/java/hw1/CalculatorSumTest.java
+++ src/test/java/hw1/CalculatorSumTest.java
@@ -1,12 +1,10 @@
 package hw1;
 
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class CalculatorSumTest extends AbstractCalculatorTest {
-
 
     @Test(dataProvider = "sumDataSetDouble", dataProviderClass = TestDataProvider.class)
     public void sumDoubleTest(double a, double b, double expected) {
@@ -14,7 +12,6 @@ public class CalculatorSumTest extends AbstractCalculatorTest {
         actual = calculator.sum(a, b);
         assertEquals(actual, expected, 0.000001);
     }
-
 
     @Test(dataProvider = "sumDataSetLong", dataProviderClass = TestDataProvider.class)
     public void sumLongTest(long a, long b, long expected) {
