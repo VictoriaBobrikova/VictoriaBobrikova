00bd9d570b3b45240fa57b7eb55ebcf58919e06a
VictoriaBobrikova <bobtory@yandex.ru>
2021-01-17 23:10:07 +0300

    hw3 fixes and add log panel

---
+++ src/test/java/hw3/page/component/differentElementsPageComponents/LogPanelComponent.java
@@ -0,0 +1,14 @@
+package hw3.page.component.differentElementsPageComponents;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class LogPanelComponent {
+
+    @FindBy(xpath = "//*[@class='panel-body-list logs']/li")
+    private List<WebElement> listLogPanel;
+
+    public List<WebElement> getListLogPanel() { return listLogPanel; }
+}
